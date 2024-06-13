fc3ce24c027c97f1f137bfec82b57462b29a9cbf
Lukeepera <Lukeeperuwu@gmail.com>
2024-06-13 20:26:07 +0400

    added error handling on URL shortener

--- utils/main_URL_short.py
+++ utils/main_URL_short.py
@@ -1,8 +1,13 @@
 import requests
 
 def url_shorten(inputed_url):
-    url = f"https://ulvis.net/API/write/get?url={inputed_url}"
-    result = requests.get(url)
-    jsoned_info = result.json()
-    shortened_url = jsoned_info["data"]["url"]
-    return shortened_url
+    try:
+        url = f"https://ulvis.net/API/write/get?url={inputed_url}"
+        result = requests.get(url)
+        jsoned_info = result.json()
+        shortened_url = jsoned_info["data"]["url"]
+
+        return shortened_url
+
+    except KeyError:
+        return "Wrong type of input. Please enter a valid URL."
